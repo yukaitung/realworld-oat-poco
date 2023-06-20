2e7edc09aa33d24a7e7afbea3df4f54fa4288f92
Kai Tung Yu <sam@bristolgram.uk>
2023-06-20 21:27:03 +0100

    update sql index

--- sql/realworld.sql
+++ sql/realworld.sql
@@ -27,6 +27,7 @@ CREATE TABLE `comments` (
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
+  KEY `article_id_index` (`article_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `tags` (
